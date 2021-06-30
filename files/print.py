931f1118d06e449f7465447c802bd4be32d21374
Danilo Righetto <danilonewtrue@gmail.com>
2021-06-30 20:07:58 -0300

    Comparing Variables

---
+++ files/print.py
@@ -0,0 +1,6 @@
+# Entendendo o comando print
+print('Comando', 'print', 'do', 'Python', 'curso da Alura', sep='-', end=" final ")
+
+pais = "Brasil"
+type(pais)
+print('O', pais, 'ganhou 5 titulos mundiais')
---
+++ files/typing.py
@@ -0,0 +1,12 @@
+# Trabalhando com tipagem no Python
+
+print('Tipagem no Python')
+
+pais = "Brasil"
+print(pais, type(pais)) # String
+ano = 2021
+print(ano, type(ano)) # Integer
+valor = 7.9
+print(valor, type(valor)) #Float
+validado = True
+print(validado, type(validado)) # Boolean
