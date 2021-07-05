aac8c1f251c3442254be38b5ecc8b0b8831cfa56
Danilo Righetto <danilonewtrue@gmail.com>
2021-07-05 19:45:48 -0300

    Format with Python

---
+++ files/format.py
@@ -0,0 +1,13 @@
+print("*********************************")
+print("*******Nova Formatação***********")
+print("*********************************")
+
+print("Tentativa {0} de {1}".format(3,10))
+
+print("R$ {}".format(1.59))
+
+print("R$ {:f}".format(1.59))
+
+print("R$ {:.2f}".format(1.59))
+
+print("R$ {:7.2f}".format(1590.90))
